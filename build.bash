1d0c5fa2287bb55de453a3f2ce770b0af737472e
arijit.khan <arijit.khan@shell.com>
2023-07-10 07:24:53 -0500

    dev: chg: updated !minor

--- build.bash
+++ build.bash
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 [[ -r /glb/apps/hpc/EasyBuild/Public/Lmod/etc/profile.d/z01_lmod-hpcti.sh ]] && . /glb/apps/hpc/EasyBuild/Public/Lmod/etc/profile.d/z01_lmod-hpcti.sh -n
+module purge
 module load HpctiSoftwareStack/PRODUCTION
 module load CMake/3.23.1-GCCcore-11.3.0
 
@@ -21,6 +22,7 @@ if [[ "${comptype}" == "amd" ]];then
 	echo "Using AMD Compilers AOCC"
 	module swap HpctiSoftwareStack/PRODUCTION HpctiSoftwareStack/ACCEPTANCE
 	module load AOCC/4.0.0-GCCcore-11.3.0
+	module load OpenMPI/4.1.4-GCC-11.3.0
 	CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release ../CompareCompilers/
 elif [[ "${comptype}" == "intel" ]];then
 	module load intel/2022a
